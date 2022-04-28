7b063c5a0e2b09b11cf35b0f4af11dab201c45c0
brikehn <thebriankwon@gmail.com>
2022-04-28 01:26:49 -0400

    need to add brew to path

--- scripts/.config/scripts/bootstrap/os/macos.sh
+++ scripts/.config/scripts/bootstrap/os/macos.sh
@@ -24,6 +24,8 @@ install_homebrew() {
   [ -f "/usr/local/bin/brew" ] || (
     print_msg "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/thebriankwon/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   )
 }
 
