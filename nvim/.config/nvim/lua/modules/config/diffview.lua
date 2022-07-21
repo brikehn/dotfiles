56d11b737c6a6a831e3de3f9f10443f0c89d3eb8
brikehn <thebriankwon@gmail.com>
2022-07-21 00:59:42 -0400

    Cleanup residual files

--- nvim/.config/nvim/lua/modules/config/diffview.lua
+++
@@ -1,8 +0,0 @@
-return function()
-  local diffview = safe_require("diffview")
-  if not diffview then
-    return
-  end
-
-  diffview.setup()
-end
--- nvim/.config/nvim/lua/modules/config/git-worktree.lua
+++
@@ -1,9 +0,0 @@
-return function()
-  local git_worktree = safe_require("git-worktree")
-  if not git_worktree then
-    return
-  end
-
-  git_worktree.setup()
-  require("telescope").load_extension("git_worktree")
-end
