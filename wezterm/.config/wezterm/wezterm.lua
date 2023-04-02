399dcd374c7cd3b016c136cab06bcea8aa1aaddb
brikehn <thebriankwon@gmail.com>
2023-04-02 17:43:29 -0500

    update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 -- Colors
-local colorscheme = require("themes/rose-pine-dawn")
+local colorscheme = require("themes/rose-pine")
 
 -- Fonts
 local font = wezterm.font("Iosevka Nerd Font Mono")
@@ -10,7 +10,7 @@ local harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- disable ligatures
 
 return {
   font = font,
-  font_size = 18.0,
+  font_size = 16.0,
   colors = colorscheme.colors(),
   window_frame = colorscheme.window_frame(),
   hide_tab_bar_if_only_one_tab = true,
