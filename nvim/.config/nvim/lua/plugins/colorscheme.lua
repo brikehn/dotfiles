5be08f8d0f73bf0fcdddbd9bc16090a97a93f884
brikehn <thebriankwon@gmail.com>
2024-06-16 13:11:50 -0400

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,27 @@
 return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    init = function()
-      vim.opt.background = 'light'
-      vim.cmd.colorscheme("rose-pine")
-    end
-  },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		opts = {
+			dim_inactive_windows = true,
+			highlight_groups = {
+				TelescopeBorder = { fg = "overlay", bg = "overlay" },
+				TelescopeNormal = { fg = "subtle", bg = "overlay" },
+				TelescopeSelection = { fg = "text", bg = "highlight_med" },
+				TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+				TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+
+				TelescopeTitle = { fg = "base", bg = "love" },
+				TelescopePromptTitle = { fg = "base", bg = "pine" },
+				TelescopePreviewTitle = { fg = "base", bg = "iris" },
+
+				TelescopePromptNormal = { fg = "text", bg = "surface" },
+				TelescopePromptBorder = { fg = "surface", bg = "surface" },
+			},
+		},
+		init = function()
+			vim.opt.background = "light"
+			vim.cmd.colorscheme("rose-pine")
+		end,
+	},
 }
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 
 -- Colors
 local colorscheme = require("themes/rose-pine-dawn")
@@ -8,12 +9,20 @@ local font = wezterm.font("JetBrainsMono Nerd Font Mono")
 
 local harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- disable ligatures
 
+local default_prog = { "/bin/zsh", "-l", "-c", "tmux attach || tmux" }
+
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
 return {
 	font = font,
-	font_size = 16.0,
+	font_size = 14.0,
 	colors = colorscheme.colors(),
 	window_frame = colorscheme.window_frame(),
 	hide_tab_bar_if_only_one_tab = true,
 	use_fancy_tab_bar = false,
 	harfbuzz_features = harfbuzz_features,
+	default_prog = default_prog,
 }
