508d30e94c521786b2bf0222cba4da8e97f42f59
brikehn <thebriankwon@gmail.com>
2024-06-22 18:59:05 -0400

    updates

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,8 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		opts = {},
 		config = function()
+			vim.keymap.set("n", "gl", vim.diagnostic.open_float)
+
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(event)
