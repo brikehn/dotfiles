ea79e9e150c27da8f6c7c9645df94a0974053d50
brikehn <thebriankwon@gmail.com>
2026-05-09 10:52:15 -0400

    fix(nvim): cleanup unused global fn

--- dot_config/nvim/lua/plugins/file-explorer.lua
+++ dot_config/nvim/lua/plugins/file-explorer.lua
@@ -7,17 +7,6 @@ return {
 			{ "-", "<CMD>Oil<CR>", mode = { "n" }, { desc = "Open parent directory" } },
 		},
 		config = function()
-			function _G.get_oil_winbar()
-				local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-				local dir = require("oil").get_current_dir(bufnr)
-				if dir then
-					return vim.fn.fnamemodify(dir, ":~")
-				else
-					-- If there is no current directory (e.g. over ssh), just show the buffer name
-					return vim.api.nvim_buf_get_name(0)
-				end
-			end
-
 			-- helper function to parse output
 			local function parse_output(proc)
 				local result = proc:wait()
